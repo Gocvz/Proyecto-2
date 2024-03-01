6c749e5b918bf772eb78b6a9a7fc637b44a4315e
Diego <diegocavazosxp@gmail.com>
2024-03-01 11:12:10 -0600

    commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+window.onscroll=function(){scrollFunction()};//Cuando cargue un documento en la ventana y haga scroll dispara 
+
+//Función vacía
+function scrollFunction(){
+    const navbar = document.querySelector('.navbar'); //Seleccionando el navbar que se encuentra dentro
+    //de una variable local, se llama local porque solamente sirve dentro de una función 
+
+    if(document.body.scrollTop>50 || document.documentElement.scrollTop>50){ //Esta palabra sirve para evaluar una cosa
+     
+        navbar.classList.add('bg-primary'); //añade la clase bg primario al nodo navbar
+        navbar.classList.remove('bg-transparent'); //quita la clase bg transparente del nodo navbar
+
+    }else{
+
+        navbar.classList.remove('bg-primary'); //añade la clase bg primario al nodo navbar
+        navbar.classList.add('bg-transparent'); //quita la clase bg transparente del nodo navbar
+
+    }
+
+}
